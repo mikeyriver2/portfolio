c8d406ab016f2e91105ae8936f5209eba4af1731
mikeyriver2 <mikeyriver2@gmail.com>
2022-09-25 22:37:14 +0800

    Ini Sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Curve } from './assets/images/Curve.svg';
+import { ReactComponent as Cloud } from './assets/images/Cloud.svg'; 
+import CustomCurve from './reusables/CustomCurve';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mikeyriver">
+      <section className="mikeyriver__nav">
+
+      </section>
+      <section className="mikeyriver__basic">
+        <Cloud />
+
+      </section>
+      <section className="mikeyriver__music">
+        <Curve />
+
+      </section>
+      <section className="mikeyriver__resume">
+        <CustomCurve />
+
+      </section>
+      <section className="mikeyriver__form">
+        
+      </section>
     </div>
   );
 }
